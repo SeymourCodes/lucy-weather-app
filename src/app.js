001ba5956195b29a3cc7d88f2836d701fd3ade1c
SeymourCodes <119512926+SeymourCodes@users.noreply.github.com>
2023-01-31 19:20:04 +0000

    test temperature

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 function displayTemperature(response) {
   console.log(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.name;
 }
 let apiKey = "o81b0274593fa059b1746c53d9te3d9d";
-let apiURL = `https://api.shecodes.io/weather/v1/current?query=Bath&key=${apiKey}&units=metric`;
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=Nepal&key=${apiKey}&units=metric`;
 axios.get(apiURL).then(displayTemperature);
