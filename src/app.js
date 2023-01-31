d2df50f520260c75f28aeb6868542deb3179d2f9
SeymourCodes <119512926+SeymourCodes@users.noreply.github.com>
2023-01-31 20:43:54 +0000

    added date and time and format date function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours} :${minutes}`;
+}
 function displayTemperature(response) {
   console.log(response.data.temperature.current);
   let description = response.data.condition.description;
@@ -7,11 +30,13 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "o81b0274593fa059b1746c53d9te3d9d";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=Sydney&key=${apiKey}&units=metric`;
