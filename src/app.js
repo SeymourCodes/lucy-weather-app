1e275c2267d72a9bd9b08d833d75d2fc353c4a21
SeymourCodes <119512926+SeymourCodes@users.noreply.github.com>
2023-01-31 20:17:13 +0000

    test

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = description;
   humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
 }
 let apiKey = "o81b0274593fa059b1746c53d9te3d9d";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=Sydney&key=${apiKey}&units=metric`;
