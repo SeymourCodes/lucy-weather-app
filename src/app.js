f6b0b50ac228b94bf656e9e2a98f7997c41dd683
SeymourCodes <119512926+SeymourCodes@users.noreply.github.com>
2023-01-31 18:16:19 +0000

    test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-alert("hello");
+function displayTemperature(response) {
+  console.log(response.data.daily.temperature.day);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(
+    response.data.daily.temperature.day
+  );
+}
+let apiKey = "o81b0274593fa059b1746c53d9te3d9d";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=Bath&key=${apiKey}&units=metric`;
+axios.get(apiURL).then(displayTemperature);
