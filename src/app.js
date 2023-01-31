e65dcd67246104b325338bbb9cae08f070fc4726
SeymourCodes <119512926+SeymourCodes@users.noreply.github.com>
2023-01-31 19:21:26 +0000

    test

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = response.data.city;
 }
 let apiKey = "o81b0274593fa059b1746c53d9te3d9d";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=Nepal&key=${apiKey}&units=metric`;
