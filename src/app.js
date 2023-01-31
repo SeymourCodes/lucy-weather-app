fd1142bfb80991e6bd131452fc59a2554dfb1e50
SeymourCodes <119512926+SeymourCodes@users.noreply.github.com>
2023-01-31 20:18:38 +0000

    rounded up the windspeed

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = description;
   humidityElement.innerHTML = response.data.temperature.humidity;
-  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "o81b0274593fa059b1746c53d9te3d9d";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=Sydney&key=${apiKey}&units=metric`;
