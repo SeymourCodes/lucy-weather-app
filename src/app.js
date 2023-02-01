f32f5f594fcf07fa98e7f2a78724ce5b6b572beb
SeymourCodes <119512926+SeymourCodes@users.noreply.github.com>
2023-02-01 16:21:11 +0000

    test

--- src/app.js
+++ src/app.js
@@ -39,6 +39,7 @@ function displayForecast() {
                                 <span class="weather-forecast-temperature-max">18°</span>
                                 <span class="weather-forecast-temperature-min">12°</span>
                             </div>
+                            </div>
                         </div>`;
   });
 
