4f1b56e78e678f48f6abefcc1cc6bc08b510dce7
SeymourCodes <119512926+SeymourCodes@users.noreply.github.com>
2023-02-01 17:30:55 +0000

    test

--- src/app.js
+++ src/app.js
@@ -47,9 +47,11 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-function getForecast (coordinates){
-console.log (coordinates);
-
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -75,8 +77,7 @@ function displayTemperature(response) {
   iconElement.setAttribute("src", response.data.condition.icon_url);
   iconElement.setAttribute("alt", response.data.condition.icon);
 
-  function getForecast (response.data.coord);
-
+  getForecast(response.data.coord);
 }
 
 function search(city) {
