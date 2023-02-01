e098c39487d8157de7c08ee29e1b6397dc16c271
SeymourCodes <119512926+SeymourCodes@users.noreply.github.com>
2023-02-01 18:26:13 +0000

    test

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "o81b0274593fa059b1746c53d9te3d9d";
-  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon={coordinates.longitude}&lat={coordinates.latitude}&key=${apiKey}units=metric"`;
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon={coordinates.lon}&lat={coordinates.lat}&key=${apiKey}units=metric"`;
   axios.get(apiURL).then(displayForecast);
 }
 
